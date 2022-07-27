8ff181f7f76bebf53a3e657342b55c87ff8827f7
Keisuke <a2201ki@aiit.ac.jp>
2022-07-28 01:56:20 +0900

    testgit add -A

---
+++ test_hello.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ERROR_EXIT(){
+	echo "$1" >$2
+	rm -f /tmp/$$-*
+	exit 1
+}
+
+
+./hello.sh > /tmp/$$-result
+
+echo "hello" > /tmp/$$-ans
+echo "world" >> /tmp/$$-ans
+
+
+diff /tmp/$$-result /tmp/$$-ans || ERROR EXIT  "1個目のテスト"
+
+echo "テストクリア"
+rm /tmp/$$-*
